46749c31dccf27bb9de29931e5d6d4b889251049
Chris Harwood <cmharwood79@gmail.com>
2024-05-03 23:37:20 -0500

    added const new employee, employees.push, calculatedMonthly, dividedMonthly

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ onReady();
 
 function onReady() {
   console.log('Javascript is working');
+
+  // calculateMonthlyTotal(); // This will be the calculated monthly total
 }
-totalMonthly = 0;
+let employees = [];
+let totalMonthly = 0;
 
 function addEmployee(event) {
   event.preventDefault();
   // console.log('employeeees');
+
   const firstName = document.getElementById('first-name');
   const lastName = document.getElementById('last-name');
   const empId = document.getElementById('emp-id');
@@ -16,24 +20,55 @@ function addEmployee(event) {
   // console.log(
   //   `${firstName.value}, ${lastName.value}, ${empId.value}, ${empTitle.value}, ${annualSalary.value}`
   // );
+
+  const newEmployee = {
+    first: firstName.value,
+    last: lastName.value,
+    id: empId.value,
+    title: empTitle.value,
+    salary: Number(annualSalary.value),
+  };
+
   document.getElementById('table-body').innerHTML += `
   <tr>
   <td>${firstName.value}</td>
   <td>${lastName.value}</td>
   <td>${empId.value}</td>
   <td>${empTitle.value}</td>
-  <td>${annualSalary.value}</td>
-  <td><button onclick='deleteEmployee(event)'></button></td>
+  <td>$${annualSalary.value}</td>
+  <td><button onclick='deleteEmployee(event)'>Delete</button></td>
   </tr>
   `;
+  console.log('salary: ', annualSalary.value);
 
+  employees.push(newEmployee);
+  console.log('employees:', employees);
+
+  // document.getElementById('empInput').reset(); // reset the entire form using the form id (html)
   // firstName.value = '';
   // lastName.value = '';
   // empId.value = '';
   // empTitle = '';
   // annualSalary = '';
+  // console.log(calculateMonthlyTotal);
+
+  calculateMonthly();
 }
 
 function deleteEmployee(event) {
   event.target.closest('tr').remove();
+  // const removeEmp = event.target;
+  // const subtractSalary = removeEmp.closest('tr').remove();
+}
+
+function calculateMonthly() {
+  let totalMonthly = 0;
+  for (let i = 0; i < employees.length; i++) {
+    totalMonthly = totalMonthly + employees[i].salary; // .salary comes from newEmployee
+  }
+  let dividedMonthly = totalMonthly / 12;
+  console.log('total monthly:', dividedMonthly);
+
+  // const totalDivideMonthly = document.getElementById('monthly-salaries');
+  // totalDivideMonthly.innerText = `${dividedMonthly}`;
 }
